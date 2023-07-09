37db23d058b7215c3b5f7dd018422ed8f5b40211
Hamza <muhammadhamzam1486@gmail.com>
2023-07-09 13:13:52 +0500

    save displaycard (reformat)

--- app/components/showcase/DisplayCard.tsx
+++ app/components/showcase/DisplayCard.tsx
@@ -9,6 +9,7 @@ const DisplayCard = ({
     description: string;
     image: string;
 }) => {
+
   return (
     <article className="display-card flex flex-col justify-center w-full h-full py-4 gap-4 sm:flex-row sm:gap-x-6 sm:items-center sm:mb-8">
         <Image src={image} alt={title} className="w-full object-cover rounded-md" width={640} height={427} />
