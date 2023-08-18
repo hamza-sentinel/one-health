5923359b8a485a3df7a1f030ab19cfc455ae63f1
Hamza <muhammadhamzam1486@gmail.com>
2023-08-18 19:00:37 +0500

    implement api in national collaborators

--- app/(client)/(links)/national-collaborators/page.tsx
+++ app/(client)/(links)/national-collaborators/page.tsx
@@ -1,35 +1,14 @@
 import Collaborator from "@/app/components/Collaborator";
+import { getData } from "@/app/utils";
 import React from "react";
 
-function NationalCollaborators() {
-  const collaborators = [
-    {
-      name: "Prof. Dr. Timothy Geary",
-      university:
-        "Institute of Parasitology, Parasitology Building, McGill University, Macdonald Campus, 21,111 Lakeshore Road, Sainte-Anne-de-Bellevue, Quebec H9X 3V9, CANADA",
-      telephone: "+1 514 398 7612",
-      fax: "+1 514 398 7857",
-      email: "timothy.g.geary@mcgill.ca",
-      image: "/timothy-geary.jpeg",
-    },
-    {
-      name: "Prof. (Emeritus) Thomas L. Rost",
-      university:
-        "Plant Biology, College of Biological Sciences, University of California, One Shields Avenue, Davis, CA-95616",
-      telephone: "530-848-5640",
-      email: "tlrost@ucdavis.edu",
-      image: "/thomas.jpeg",
-    },
-    {
-      name: "Nancy J. Allen",
-      university:
-        "MBA, Ph.D., Global Engagement Office, College of Agriculture & Environmental Sciences, University of California, Davis, Davis, CA 95616, USA",
-      telephone: "1-530-754-0628",
-      cell: "1-530-574-7235",
-      email: "njallen@ucdavis.edu",
-      image: "/nancy.jpeg",
-    },
-  ];
+async function getCollaborators() {
+  const data = await getData(process.env.URL + "/api/national-collaborators");
+  return data;
+}
+
+async function NationalCollaborators() {
+  const collaborators = await getCollaborators();
   return (
     <section>
       <div className="container">
