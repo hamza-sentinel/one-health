46fdaa922a22571ef9ee660b6444cb0eeee50cef
Hamza <muhammadhamzam1486@gmail.com>
2023-12-25 12:58:17 +0500

    ADD open cors

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,6 +1,28 @@
 // import { withAuth } from "next-auth/middleware";
 
+import { NextResponse } from "next/server";
 export { default } from "next-auth/middleware";
+
+export function middleware() {
+  const res = new NextResponse();
+
+  res.headers.append("Access-Control-Allow-Credentials", "true");
+  res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+  res.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,DELETE,PATCH,POST,PUT"
+  );
+  res.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  return res;
+}
+
+export const config = {
+  matcher: "/api/:path*",
+};
 
 // export default withAuth(function middleware(req) {}, {
 //   callbacks: {
